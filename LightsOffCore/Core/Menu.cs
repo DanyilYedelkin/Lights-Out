426ecc309b87dd9fde44dd90904018037da666e9
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2022-03-18 20:01:19 +0100

    Add new file Menu.cs (for manipulation with main menu)

---
+++ LightsOffCore/Core/Menu.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace LightsOffCore.Core
+{
+    public class Menu
+    {
+        private int SelectedIndex;  // for current cursor position 
+        private string[] Options;   // command buttons
+        private string Prompt;      // game's logo
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 0;  // cursor start position
+        }
+
+        private void DisplayOptions()
+        {
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];  // a current command button
+                string prefix;                      // for checking, where our cursor is now located
+
+                if (i == SelectedIndex) // if a cursor on a button
+                {
+                    prefix = "*";
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else // if not
+                {
+                    prefix = " ";
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+
+                WriteLine($"{prefix} << {currentOption} >>");
+            }
+
+            ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                // Update SelectedIndex based on arrow keys.
+                if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
+    }
+}
