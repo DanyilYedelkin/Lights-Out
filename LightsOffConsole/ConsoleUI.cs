6a40393c44abb3b64b356028026feb9f95783546
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2022-03-18 19:54:32 +0100

    Update ConsoleUI.cs (add new ideas and comments)

--- LightsOffConsole/ConsoleUI.cs
+++ LightsOffConsole/ConsoleUI.cs
@@ -1,36 +1,39 @@
 using LightsOff.Core;
 using LightsOffCore.Core;
+using LightsOffCore.Entity;
 using LightsOffCore.Service;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Console;
 
 
 namespace LightsOff.ConsoleUI
 {
     public class ConsoleUI
     {
-        private readonly Field _field;
-        ChangeLights changeLights;
+        private readonly Field _field;      // field (map)
+        private DisplayInfo displayInfo;    // all dispay information
+        private ChangeLights changeLights;  // changing lights
         private readonly IScoreService _scoreService = new ScoreServiceFile();
 
         public ConsoleUI(Field field)
         {
             _field = field;
             changeLights = new ChangeLights(_field);
+            displayInfo = new DisplayInfo();
         }
 
         public void Play()
         {
             PrintTopScores();
 
-
-            while (!IfWin())
+            while (!_field.IfWin())
             {
                 PrintField();
                 try
                 {
-                    var coordinates = ConsoleInput();
+                    var coordinates = ConsoleInput();   // input coordinates to change lights' type
 
                     try
                     {
@@ -38,42 +41,52 @@ namespace LightsOff.ConsoleUI
                     }
                     catch (IndexOutOfRangeException indexOutOfMap)
                     {
-
-                        Console.WriteLine("Out of bounds, try again !!!");
+                        WriteLine("Out of bounds, try again !!!");
                     }
                 }
                 catch (FormatException invalidTypeVariable)
                 {
-
-                    Console.WriteLine("Please type integers !!!");
+                    WriteLine("Please type integers !!!");
                 }
 
-                Console.WriteLine();
+                WriteLine();
             }
 
-            _scoreService.AddScore(
-                new LightsOffCore.Entity.Score { Player = "Jaro", Points = _field.GetScore(), PlayedAt = DateTime.Now });
+            PrintField();
+            WriteLine();
 
-            EndMessage();
+            // add new player with his scores into a score table
+            _scoreService.AddScore(
+                new Score { Player = Environment.UserName, Points = _field.GetScore(), PlayedAt = DateTime.Now });
+
+            bool repeat = LastMessage();
+            if (repeat == true) // for repeat a game 
+            {
+                _field.Initialize();
+                Clear();
+                Play();
+            }
         }
+
 
         private void PrintField()
         {
-            Console.Write(ToString(_field));
-            Console.WriteLine("\nScore: {0}\n", _field.GetScore());
+            Write(ToString(_field));
+            WriteLine("\nScore: {0}\n", _field.GetScore());
         }
 
-        private String ToString(Field field)
+        private string ToString(Field field)    // translate each points of field to string
         {
             if (field is null)
             {
                 throw new ArgumentNullException(nameof(field));
             }
 
-            String s = "";
+            string s = "";
 
             for (var row = 0; row < field.RowCount; row++)
             {
+                s += "\t";
                 for (var column = 0; column < field.ColumnCount; column++)
                 {
                     if (field[row, column].Value)
@@ -92,47 +105,51 @@ namespace LightsOff.ConsoleUI
 
         private Tuple<int, int> ConsoleInput()
         {
-            Console.WriteLine("Enter X: ");
-            int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Y: ");
-            int y = int.Parse(Console.ReadLine());
+            WriteLine("Enter X: ");
+            int x = int.Parse(ReadLine());
+            WriteLine("Enter Y: ");
+            int y = int.Parse(ReadLine());
 
             return Tuple.Create(x, y);
         }
 
-        private bool IfWin()
-        {
-            bool win = true;
-
-            for (var row = 0; row < _field.RowCount; row++)
-            {
-                for (var column = 0; column < _field.ColumnCount; column++)
-                {
-                    if (_field[row, column].Value) win = false;
-                }
-            }
-
-            return win;
-        }
-
-        private void EndMessage()
-        {
-            if (IfWin())
-            {
-                Console.WriteLine("Congratulations, you WIN !!!!");
-            }
-        }
-
         private void PrintTopScores()
         {
-            Console.WriteLine("----------------------------------------------------------------");
-            Console.WriteLine("------------------------    TOP SCORES   -----------------------");
-            Console.WriteLine("----------------------------------------------------------------");
+            int position = 1;
+            WriteLine("----------------------------------------------------------------");
+            WriteLine("------------------------    TOP SCORES   -----------------------");
 
             foreach (var score in _scoreService.GetTopScores())
             {
-                Console.WriteLine("{0} {1}\n", score.Player, score.Points);
+                WriteLine(position + ". {0} {1}", score.Player, score.Points);
+                position++;
+            }
+
+            WriteLine("----------------------------------------------------------------");
+        }
+        private bool LastMessage()
+        {
+            WriteLine("\tCongratulations, you WIN !!!!\n");
+
+            while (true)
+            {
+                WriteLine("\n\tDo you want to restart the game? Input: [yes]/[no]");
+                string restart = ReadLine().ToLower();
+
+                switch (restart)
+                {
+                    case "yes":
+                        return true;
+                    case "no":
+                        WriteLine("\n\tThanks for playing the game. I wish you will have a good day :D\n");
+                        displayInfo.ExitGame();
+                        return false;
+                    default:
+                        WriteLine("\n\tPlease enter the correct command\n");
+                        break;
+                }
             }
         }
+
     }
 }
