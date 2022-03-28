522264d77cdb94e5ea3fcd26887199b9aac07a9d
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2022-03-29 00:02:18 +0200

    Add new file Rating.cs

---
+++ LightsOffCore/Entity/Rating.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace LightsOffCore.Entity
+{
+    [Serializable]
+    public class Rating
+    {
+        public string Player { get; set; }
+
+        public double Ratings { get; set; }
+    }
+}
