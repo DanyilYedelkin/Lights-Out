0798ab10bc2a5479b66f32abe51a2abd895e44d4
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2022-03-29 00:01:43 +0200

    Add new file Comment.cs

---
+++ LightsOffCore/Entity/Comment.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LightsOffCore.Entity
+{
+    [Serializable]
+    public class Comment
+    {
+        public string Player { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
